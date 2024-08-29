9e42ac99a1603cc7f166095361678a46cd489bd2
Elham13600 <bardiaxxo@gmail.com>
2024-08-29 20:45:37 +0200

    showed broadcasts on the home/start page

--- Broadcast-socialmedia/Broadcast-SocialMedia/Broadcast-SocialMedia/Controllers/HomeController.cs
+++ Broadcast-socialmedia/Broadcast-SocialMedia/Broadcast-SocialMedia/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Broadcast_SocialMedia.Models;
 using Broadcast_SocialMedia.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Broadcast_SocialMedia.Controllers
 {
@@ -26,11 +27,19 @@ namespace Broadcast_SocialMedia.Controllers
             var user = await _userManager.GetUserAsync(User);
             var dbUser = await _dbContext.Users.Where(u => u.Id == user.Id).FirstOrDefaultAsync();
 
-            var listeningTo = await _dbContext.Users.Where(u => u.Id == user.Id)
+            var broadcasts = await _dbContext.Users.Where(u => u.Id == user.Id)
                .SelectMany(u => u.ListeningTo)
+               .SelectMany (u => u.BroadCasts)
+               .Include(b => b.User)
+               .OrderByDescending(b => b.Published)
             .ToListAsync();
 
-            return View();
+            var viewModel = new HomeIndexViewModel()
+            {
+                Broadcasts = broadcasts,
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
--- Broadcast-socialmedia/Broadcast-SocialMedia/Broadcast-SocialMedia/ViewModels/HomeIndexViewModel.cs
+++ Broadcast-socialmedia/Broadcast-SocialMedia/Broadcast-SocialMedia/ViewModels/HomeIndexViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Broadcast_SocialMedia.ViewModels
+﻿using Broadcast_SocialMedia.Models;
+
+namespace Broadcast_SocialMedia.ViewModels
 {
 	public class HomeIndexViewModel
 	{
-	}
+        public List<BroadCast> Broadcasts { get; set; }
+    }
 }
